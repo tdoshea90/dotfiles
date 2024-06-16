8b1a71fa788b7f9a3f1df9321794950879ab09bf
Tim O <tdoshea90@yahoo.com>
2024-06-16 19:24:39 +0000

    Testing default shell behavior

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Use the default zsh located in /home/codespace/.oh-my-zsh
 echo "Setting up Codespace dotfiles..."
 
-sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
+#sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 #source $HOME/.zshrc
 
 echo "Done setting up Codespace dotfiles
