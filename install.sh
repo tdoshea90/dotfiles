20fed4ce809be1d250c668169cd78d2e6cb862d7
Tim O <tdoshea90@yahoo.com>
2024-06-16 19:48:28 +0000

    Add missing quote

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 # Use the default zsh located in /home/codespace/.oh-my-zsh
 echo "Setting up Codespace dotfiles..."
 
-ln -s aliases.zsh /home/codespace/.oh-my-zsh/custom
+ln -s aliases.zsh $HOME/.oh-my-zsh/custom
 sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 #source $HOME/.zshrc
 
-echo "Done setting up Codespace dotfiles
+echo "Done setting up Codespace dotfiles"
